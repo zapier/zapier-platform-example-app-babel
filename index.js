4445ed3a4ac1475a4c3ba01a0be52a2cf4f205a1
Fokke Zandbergen <mail@fokkezb.nl>
2018-04-16 14:18:00 +0200

    single quote

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env.BABEL_DISABLE_CACHE = 1;
 
-global._babelPolyfill || require("babel-polyfill");
+global._babelPolyfill || require('babel-polyfill');
 
 module.exports = require('./lib');
